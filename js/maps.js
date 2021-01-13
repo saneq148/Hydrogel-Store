63b25528a1413fbe0c378ecbc7ae2ee17c09f53f
Aleksandr Nikitin <sashanikitin148@yandex.ua>
2021-01-13 21:02:26 +0200

    Fixed gmap

--- js/maps.js
+++ js/maps.js
@@ -3,26 +3,23 @@ function initMap() {
         zoom: 15,
         center: { lat: 50.783026010700000, lng: 25.364062786100000 },
     });
-    const beachMarker = new google.maps.Marker({
-        position: { lat: 50.783026010700000, lng: 25.364062786100000 },
-        map,
-    });
 }
 
 function setMapCoords(wareHouses) {
     const map = new google.maps.Map(document.getElementById("google-map"), {
-        zoom: 10,
-        center: { lat: 50.783026010700000, lng: 25.364062786100000 },
+        zoom: 8,
+        center: { lat: parseFloat(wareHouses[0].Latitude), lng: parseFloat(wareHouses[0].Longitude) },
+        disableDefaultUI: true,
     });
     const image = {
         url:
-            "https://novaposhta.ua/uploads/misc/img/ico/punkt_priema.png",
+            "https://gidrogel.store/assets/icons/marker.png",
         // This marker is 20 pixels wide by 32 pixels high.
-        size: new google.maps.Size(20, 32),
+        size: new google.maps.Size(44, 40),
         // The origin for this image is (0, 0).
         origin: new google.maps.Point(0, 0),
         // The anchor for this image is the base of the flagpole at (0, 32).
-        anchor: new google.maps.Point(0, 32),
+        anchor: new google.maps.Point(0, 40),
     };
     for (let i = 0; i < wareHouses.length; i++) {
         const warehouse = wareHouses[i];
@@ -36,10 +33,10 @@ function setMapCoords(wareHouses) {
                 text: warehouse.Number,
                 color: "#000000",
                 fontSize: "16px",
-                fontWeight: "bold"
+                //fontWeight: "bold"
             },
             zIndex: parseInt(warehouse.Number),
         });
-
+        marker.addListener("hover")
     }
 }
