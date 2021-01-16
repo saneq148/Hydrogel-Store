29d58a8b843ff76108cf4f6ece810bf8518b1303
Aleksandr Nikitin <sashanikitin148@yandex.ua>
2021-01-16 23:07:38 +0200

    Test

--- testorder.php
+++ testorder.php
@@ -64,15 +64,7 @@ $cartInfo = "
     Коментар: {$info}
 </div>";
 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-$body = $cartBody.$cartInfo.$ip;
+$body = $cartBody.$cartInfo.$_SERVER['REMOTE_ADDR'];
 echo $body;
 
 
